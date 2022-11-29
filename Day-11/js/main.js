9010e9d39026f5171564decf1bc8fe95dab6a29a
Khangaikhuu <khangaikhuu@gmail.com>
2022-11-29 09:48:59 +0800

    feat: adding day 11

---
+++ Day-11/js/main.js
@@ -0,0 +1,35 @@
+var baby = "Baby Shark, doo-doo, doo-doo";
+
+console.log(baby);
+
+console.log(baby);
+console.log(baby);
+console.log(baby);
+
+
+
+var a = "Baby Shark, ";
+var b = "doo-doo, ";
+var c = "doo-doo";
+var d = "Mommy Shark, ";
+
+console.log(a + b + c);
+console.log(a + b + c);
+console.log(a + b + c);
+
+
+let myNumber = 5;
+let undify;
+let nullVariable = null;
+let myDecision = true;
+
+// typeof
+console.log(typeof 1);
+console.log(typeof "Hello");
+console.log(typeof undify);
+console.log(typeof nullVariable);
+console.log(myDecision);
+console.log(typeof myDecision);
+
+
+
