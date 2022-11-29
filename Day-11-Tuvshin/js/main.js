d87db1bace5566088b1979a33ab449e1612ac4da
Khangaikhuu <khangaikhuu@gmail.com>
2022-11-29 14:32:31 +0800

    adding tuvshin day 11

---
+++ Day-11-Tuvshin/js/main.js
@@ -0,0 +1,25 @@
+var a = 1;
+console.log(a);
+
+console.log(typeof(1.6))
+
+// ene code n eniig hiideg
+/**
+ * dfadf
+ */
+
+console.log(a * b)
+
+var b = 1
+var b = 2
+
+let c = 1
+c = 2
+
+const d = 'Hio';
+
+const PI = 3.14;
+
+// PI * R * R 
+
+// 2 * PI * R
