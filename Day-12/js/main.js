2eeb9f9ec991cd9305f63d2bad94a50c32b13bec
Khangaikhuu <khangaikhuu@gmail.com>
2022-11-30 10:26:00 +0800

    adding more string operations to the day 12

--- Day-12/js/main.js
+++ Day-12/js/main.js
@@ -1,31 +1,86 @@
-let a = "hi";
-let b = "hi";
+// let a = "hi";
+// let b = "hi";
 
-console.log(a < b);
+// console.log(a < b);
 
-console.log(a == b);
+// console.log(a == b);
 
-console.log(a === b);
+// console.log(a === b);
 
-let c = 10;
-let d = '10';
-console.log(c == d);
-console.log(c === d);
-console.log(c !== d);
+// let c = 10;
+// let d = '10';
+// console.log(c == d);
+// console.log(c === d);
+// console.log(c !== d);
 
 
-let e = true;
-let f = false;
+// let e = true;
+// let f = false;
 
-console.log(e < f);
-console.log(e > f);
+// console.log(e < f);
+// console.log(e > f);
 
-let result = 10;
+// let result = 10;
 
-if (result > 10) {
-    console.log('more than 10');
-} else {
-    console.log('less than 10');
-}
+// if (result > 10) {
+//     console.log('more than 10');
+// } else {
+//     console.log('less than 10');
+// }
 
-result > 10 ? (result < 11 ? console.log(test): console.log('hi')) : console.log('less than 10');
+// result > 10 ? (result < 11 ? console.log('test') : console.log('hi')) : console.log('less than 10');
+
+// // 3000, 27500, 100000
+// const tip1 = 3000;
+// const tip2 = 27500;
+// const tip3 = 100000;
+
+// if (5000 >= tip1 && 30000 <= tip1) {
+//     console.log('bodlogo bodoh 15%');
+// } else {
+//     console.log('busad bodoh 20%');
+// }
+
+// let tipResult2 = (5000 >= tip2 && 30000 <= tip2) ? tip2 * 0.15 + tip2 : tip2 * 0.2 + tip2;
+// console.log(tipResult2);
+
+// const input = prompt('Give me your money');
+// const numberInput = Number(input);
+// // const numberInputPrompt = Number(prompt('Give me your money'));
+
+// let exTipResult = (5000 <= numberInput && 30000 >= numberInput) ? numberInput * 0.15 + numberInput : numberInput * 0.2 + numberInput;
+// alert(exTipResult);
+
+// // Onts dumd is bad in his lessons
+
+// const studentPoint = Number(prompt('Give me your score'));
+// const studentName = prompt('Give me your student name');
+
+// let studentResult = 'Оноо дандаа 100-аас доош байх ёстой';
+
+// if (studentPoint < 60) {
+//     studentResult =  'Маш муу'
+// } else if (studentPoint >= 60 && studentPoint < 70) {
+//     studentResult = 'Хангалттай'
+// }
+// else if (studentPoint >= 70 && studentPoint < 80) {
+//     studentResult = 'Дунд'
+// }
+// else if (studentPoint >= 80 && studentPoint < 90) {
+//     studentResult = 'Сайн'
+// }
+// else if (studentPoint >= 90 && studentPoint < 100) {
+//     studentResult = 'Маш сайн'
+// }
+// else {
+//     console.log('buruu')
+// }
+
+
+// alert(studentName + ' - ' + studentPoint + ' - ' + studentResult);
+
+
+// console.log(`${studentName} - ${studentPoint} - ${studentResult}`);
+
+
+alert ("Lorem Ipsum \"Its double quotation\" is simply dummy text of the printing and typesetting industry. \n\n \t Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
