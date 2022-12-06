28ca364a8886a240969e6216d94949fc822e36f8
Khangaikhuu <khangaikhuu@gmail.com>
2022-12-06 10:36:15 +0800

    feat: adding day 16 with 2d arrays

--- Day-16/js/2D-Arrays.js
+++ Day-16/js/2D-Arrays.js
@@ -28,20 +28,32 @@ console.log(row1);
 console.log(row2);
 console.log(row3);
 
-let tictactoe = [['x', 'o', 'x'], ['o', 'o', 'x'], ['x', 'x', 'o'] ];
+let tictactoe = 
+[   ['x', 'o', 'x'], 
+    ['o', 'o', 'x'], 
+    ['x', 'x', 'o'] 
+];
 console.log(tictactoe);
 console.log(tictactoe[1])
 console.log(tictactoe[1][1])
+
+tictactoe[0][1] = 'x';
+
+// input -ээр тухайн tictactoe-ийн баганын болоод мөрийн индэксийг 
+// дарааллуулж аваад 3 дахь удаад нь юугаар солихийг нь өгнө.
 
 console.log('for loop with for loop')
 // tictactoe-ийн бүх мөрний элементүүдийг хэвлэж харуулна уу
 for(let i = 0; i < tictactoe.length; i++) {
     // console.log(tictactoe[i]);
     // double FOR loops
+    let output = ''
     for(let j = 0; j < tictactoe[i].length; j++) {
-        console.log(tictactoe[i][j]);
+        output = output + "| " +  tictactoe[i][j] + " |";
     }
+    console.log(output);
 }
+
 
 
 
---
+++ Day-16/js/date.js
@@ -0,0 +1,22 @@
+console.log('date is here');
+
+let now = new Date(); 
+console.log( now );
+
+let Jan01_1970 = new Date(0);
+console.log( Jan01_1970 );
+
+let Dec31_1969 = new Date(-24 * 3600 * 1000);
+console.log( Dec31_1969 );
+
+// 2022 - 1969 = 53 * 12
+let date = new Date("2017-01-26");
+console.log(date);
+
+let Dec31_2022 = new Date("2022-12-31")
+console.log(Dec31_2022);
+let difference = Dec31_2022 - now
+console.log(difference)
+
+let time = new Date(difference);
+console.log(time.getDate()+1);
