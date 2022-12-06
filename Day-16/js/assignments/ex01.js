62ca5f6fbffeeaf51a5b51b681e96c352ea77982
Khangaikhuu <khangaikhuu@gmail.com>
2022-12-06 12:30:07 +0800

    feat: adding debugger

---
+++ Day-16/js/assignments/ex01.js
@@ -0,0 +1,16 @@
+//1. өгөгдсөн 2 тооны ихийг олох функц
+
+let num1= 99;
+let num2 = 6;
+
+
+function findMax(x, y) {
+    debugger;
+    if (x < y) {
+        console.log(y + ' is bigger than ' + x);
+    } else {
+        console.log(x + ' is bigger than ' + y);
+    }
+}
+
+findMax(num1, num2)
--- Day-16/js/main.js
+++ Day-16/js/main.js
@@ -1,14 +1,66 @@
-
-
 let numbers = [1, 4, 5, 6, 8, 10, 3, 2, 10, 9, 8];
-console.log(numbers)
+
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (isEven(numbers[i])) {
+//         numbers[i] = numbers[i] + 4;
+//     } else {
+//         numbers[i] = numbers[i] - 5;
+//     }
+// }
+console.log(numbers);
+
+function isEven(x) {
+    // console.log('the number is ' + x);
+
+    if(x % 2 == 0) {
+        // console.log("it is a even number");
+        return true;
+    } else {
+        // console.log('it is an odd number');
+        return false;
+    }
+}
+
+console.log(isEven(numbers[0]));
+console.log(isEven(numbers[1]));
+
+
+
+// isEven(5);
+// isEven(100);
+// isEven(numbers[0])
+
+
+// addTwoNumbers(x, y) = x + y
+function addTwoNumbers(x, y) {
+    return x + y;
+}
+console.log(addTwoNumbers(4, 5));
+
+function multiplyTwoNumbers(x, y) {
+    return x * y;
+}
+console.log(multiplyTwoNumbers(4, 5));
+
+function multiplyThreeNumbers (x, y, z) {
+    const m = x * y * z;
+    return m;
+}
+
+const result = multiplyThreeNumbers(4, 5, 6);
+console.log(result);
+
 
 for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 == 0) {
+    debugger
+    if (isEven(numbers[i])) {
         numbers[i] = numbers[i] + 4;
     } else {
         numbers[i] = numbers[i] - 5;
     }
 }
 console.log(numbers);
+
+console.log(Math.sqrt(4));
 
