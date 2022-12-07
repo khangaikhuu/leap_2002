c1e3df04ca9773fa028bbb9ff13db63edfc161d5
Khangaikhuu <khangaikhuu@gmail.com>
2022-12-07 12:21:14 +0800

    feat: adding array.js with array methods

--- Day-17/js/array.js
+++ Day-17/js/array.js
@@ -76,7 +76,78 @@ console.log(arrayInc.includes(2));
 console.log(arrayInc.includes(5));
 console.log(arrayInc.includes(3));
 
-// 
+// arr1 = [1, 2, 3] => 3 index? => 2
+let findIndex = function (arr, number) {
+    let index = -1;
+    for(let i = 0; i < arr.length; i++) {
+        if(number === arr[i]){
+            index = i;
+        }
+    }
+    return index;
+}
+const arrIndex = [1, 2, 3]
+console.log(findIndex(arrIndex, 3)) // => 2
+console.log(findIndex(arrIndex, 2)) // => 1
+console.log(findIndex(arrIndex, 10)) // => -1
+console.log('now it is time to test indexof method of array')
+console.log(arrIndex.indexOf(3)) // => 2
+console.log(arrIndex.indexOf(2)) // => 1
+console.log(arrIndex.indexOf(10)) // => -1
+const animals = ['chono', 'buhun', 'temee', 'nugas', 'buhun'];
+console.log(animals.indexOf('buhun')); // => 1 
+console.log(animals.indexOf('buhun', 2)); // => 4
+console.log(animals.indexOf('giraffe')); // -1
 
+// JOIN 
+console.log('JOIN ME function')
 
+// arr1 = [1, 2, 3] => 123
+// arr1 = [1, 2, 3] => 1;2;3
+let funcJoin = (arr, symbol) => {
+    let output = '';
 
+    for(let i = 0; i < arr.length; i++){
+        if(i == arr.length - 1) {
+            symbol = ''
+        }
+        output = output + arr[i] + symbol;
+    }
+
+    return output;
+}
+
+const arrayJoin = [1, 2, 3]
+console.log(funcJoin(arrayJoin, "+")) // => 1+2+3+
+console.log(funcJoin(arrayJoin, ",")) // => 1,2,3,
+console.log(funcJoin(arrayJoin, "-")) // => 1,2,3,
+
+console.log('array join method')
+const jiguurten = ['Elee', 'Heree', 'Galuu'];
+console.log(jiguurten.join()); // "Elee,Heree,Galuu"
+console.log(jiguurten.join('')); // "EleeHereeGaluu"
+console.log(jiguurten.join('-')); // "Elee-Heree-Galuu"
+
+// arr1 = [1, 2, 3] => pop => [1, 2]
+console.log('pop array function')
+let popFunc = (arr) => {
+    const lastIndex = arr.length -1 ;
+    let poppedArray = []
+
+    for(let i = 0; i < arr.length; i++){
+        if (i != lastIndex) {
+            poppedArray.push(arr[i]);
+        }
+    }
+
+    return poppedArray;
+}
+
+const popArray = [1, 2, 3]
+console.log(popFunc(popArray)) // [1, 2]
+console.log(popFunc([12, 4, 3, 5, 6])) // [12, 4, 3, 5]
+
+console.log('pop array method')
+const popArrayResult = [3, 4, 5, 6, 7];
+popArrayResult.pop()
+console.log(popArrayResult);
